9255418c1d93d8abe90e5bba8bda7eb574c65e7e
Makedoncek <123456v64@gmail.com>
2023-10-08 23:55:13 +0300

    node added

---
+++ MyCollection/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace MyCollection;
+
+public class Node <T> 
+{
+    public T Data { get; internal set; }
+    public Node<T> Next { get; internal set; }
+
+    public Node(T data)
+    {
+        Data = data;
+    }
+}
